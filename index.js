c71362267088a4f3c44bfa23e8af790af80e3084
Abhishek <warriorsshadow39@gmail.com>
2025-03-01 16:39:47 +0530

    third page added just button changes left

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ let activeStepCounter = 1;
 let stepInfoChildren = stepInfo.children;
 let contactSection = document.querySelector(".contacts");
 let formSectionContent = document.querySelector(".input-contact");
+let changablePart = document.querySelector(".changablePart");
+let backBtn = document.querySelector(".back-btn");
+
+// console.log(changablePart.children);
+console.log(stepInfoChildren);
+
 // console.log(contactSection.children[0]);
-console.log(formSectionContent);
+// console.log(formSectionContent);
+// console.log(secondPageContent);
 
 let heading = contactSection.children[0].children;
 heading = Array.from(heading);
-console.log(heading);
+// console.log(heading);
+// console.log(stepInfo);
 
 let formDataArray = [];
 let contactHeadingArray = [
@@ -42,23 +50,51 @@ function areAllInputsFilled() {
 nextStepBtn.addEventListener("click", (event) => {
   event.preventDefault();
   console.log(areAllInputsFilled());
-  if (!areAllInputsFilled()) {
+  if (!areAllInputsFilled() && activeStepCounter < 2) {
     return;
   }
 
-  if (activeStepCounter < stepInfoChildren.length) {
+  if (activeStepCounter < stepInfoChildren.length - 1) {
     let currentStep = stepInfoChildren[activeStepCounter].children[0];
+    currentStep.classList.remove("bg-step-inactive");
     currentStep.classList.add("bg-step-active");
     currentStep.children[0].style.color = "black";
     let prevStep = stepInfoChildren[activeStepCounter - 1].children[0];
     prevStep.classList.remove("bg-step-active");
     prevStep.children[0].style.color = "";
     prevStep.classList.add("bg-step-inactive");
+    let newPage = changablePart.children[activeStepCounter];
+    backBtn.classList.remove("hidden");
+    let oldPage = changablePart.children[activeStepCounter - 1];
+    newPage.classList.remove("hidden");
+    oldPage.classList.add("hidden");
     activeStepCounter++;
     headingChangeFunction(activeStepCounter - 1);
   }
 });
 
+if (backBtn && !backBtn.disabled) {
+  backBtn.addEventListener("click", () => {
+    if (activeStepCounter > 1) {
+      activeStepCounter--;
+      let currentSteperSection =
+        stepInfoChildren[activeStepCounter].children[0];
+      currentSteperSection.classList.remove("bg-step-active");
+      currentSteperSection.children[0].style.color = "";
+      currentSteperSection.classList.add("bg-step-inactive");
+      let oldSteperSection =
+        stepInfoChildren[activeStepCounter - 1].children[0];
+      oldSteperSection.classList.remove("bg-step-inactive");
+      oldSteperSection.classList.add("bg-step-active");
+      console.log(oldSteperSection);
+
+      let currentPage = changablePart.children[activeStepCounter];
+      let oldPage = changablePart.children[activeStepCounter - 1];
+      currentPage.classList.add("hidden");
+      oldPage.classList.remove("hidden");
+    }
+  });
+}
 function headingChangeFunction(num) {
   heading[0].textContent = contactHeadingArray[num]["first-heading"];
   heading[1].textContent = contactHeadingArray[num]["second-heading"];
